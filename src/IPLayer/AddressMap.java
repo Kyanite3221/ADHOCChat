11911963a170672d1187669098e53b399eb844a2
G-schmidt <g.f.c.h.schmidt@student.utwente.nl>
2017-04-10 15:10:11 +0200

    set, get, allnames methods  added to addressmap

--- src/IPLayer/AddressMap.java
+++ src/IPLayer/AddressMap.java
@@ -1,7 +1,35 @@
 package IPLayer;
 
+import java.util.*;
+
 /**
  * Created by Georg on 10-Apr-17.
  */
 public class AddressMap {
+    public HashMap<String, String> ipNameTable = new HashMap<>();
+
+    public void setIpNameTable(String ipaddress, String name) {
+        ipNameTable.put(ipaddress,name);
+    }
+
+    public boolean checkName (String name) {
+        return ipNameTable.containsValue(name);
+    }
+
+    public String getIpaddress(String name) {
+        for (Map.Entry<String,String> entry: ipNameTable.entrySet()) {
+            if (Objects.equals(entry.getValue(),name)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    public String getName(String ipaddress) {
+        return ipNameTable.get(ipaddress);
+    }
+
+    public Collection<String> allnames() {
+        return ipNameTable.values();
+    }
 }
