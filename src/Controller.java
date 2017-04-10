878d9fcb5fc96d8d2ba80e83a8d546044acd4cf3
Thomas <t.j.stein@student.utwente.nl>
2017-04-10 14:37:36 +0200

    Extend controller

--- src/Controller.java
+++ src/Controller.java
@@ -2,10 +2,8 @@ import IPLayer.IPLayer;
 import TCPLayer.*;
 import View.View;
 
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.io.IOException;
+import java.net.*;
 
 import java.util.LinkedList;
 
@@ -14,45 +12,51 @@ import java.util.LinkedList;
  */
 public class Controller {
 	private static final String ADHOC_ADDRESS = "192.168.5.0";
+	private static final int DUMMY_PORT = 3000;
 
 	public static void main(String[] args) {
+		DatagramSocket socket;
 		try {
 			InetAddress addr = InetAddress.getByName(ADHOC_ADDRESS);
-			DatagramSocket socket = new DatagramSocket();
-			socket.
+			socket = new DatagramSocket();
+			socket.connect(addr, DUMMY_PORT);
+
+
+			View view = new View();
+			Thread viewThread = new Thread(view);
+
+			TCPLayer tcpLayer = new TCPLayer();
+			IPLayer ipLayer = new IPLayer();
+
+			while (true) {
+				if (view.hasMessage()) {
+					String plaintext = view.pollMessage();
+
+					//TODO: we don't know what reciever entails yet
+					String receiver = "";
+
+					byte[] data = encodeToByteArray(plaintext);
+
+					tcpLayer.createDataMessage(data);
+					tcpLayer.createTCPMessage();
+					LinkedList<TCPMessage> tcpDataList = tcpLayer.tick();
+
+					for(TCPMessage tcpData : tcpDataList){
+						byte[] ipData = ipLayer.addIPHeader(tcpData.toByte());
+						//TODO: figure out how the fuck to send stuff aaaaahhhhh
+
+						socket.send(new DatagramPacket(ipData, ipData.length));
+					}
+
+
+				}
+			}
 		} catch (SocketException e) {
 			e.printStackTrace();
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
-		}
-
-		View view = new View();
-		Thread viewThread = new Thread(view);
-
-		TCPLayer tcpLayer = new TCPLayer();
-		IPLayer ipLayer = new IPLayer();
-
-		while (true) {
-			if (view.hasMessage()) {
-				String plaintext = view.pollMessage();
-
-				//TODO: we don't know what reciever entails yet
-				String receiver = "";
-
-				byte[] data = encodeToByteArray(plaintext);
-
-				tcpLayer.createDataMessage(data);
-				tcpLayer.createTCPMessage();
-				LinkedList<TCPMessage> tcpDataList = tcpLayer.tick();
-
-				for(TCPMessage tcpData : tcpDataList){
-					byte[] ipData = ipLayer.addIPHeader(tcpData.toByte());
-					//TODO: figure out how the fuck to send stuff aaaaahhhhh
-					send(ipData);
-				}
-
-
-			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
