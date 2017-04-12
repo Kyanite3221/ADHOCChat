c29608a3fd7256bb91753954bfe687a503fe35d4
G-schmidt <g.f.c.h.schmidt@student.utwente.nl>
2017-04-12 13:28:05 +0200

    try to fix routingprotocol

--- src/Routing/RoutingProtocol.java
+++ src/Routing/RoutingProtocol.java
@@ -1,17 +1,16 @@
 package Routing;
 
 
+import IPLayer.IPLayer;
+
 import java.util.HashMap;
 
 /**
  * Created by Georg on 11-Apr-17.
  */
-public class RoutingProtocol implements Runnable {
-    private HashMap<Integer, Route> forwardingTable= new HashMap<>();
-
-    @Override
-    public void run() {
-        int myAddress;
-    }
-
+public class RoutingProtocol {
+    private HashMap<Byte[], MyRoute> forwardingTable= new HashMap<>();
+    IPLayer ipLayer = new IPLayer();
+    byte[] myAddress = ipLayer.ipStringToByteArray(ipLayer.getOwnIP());
+    MyRoute ownLocation = new MyRoute();
 }
